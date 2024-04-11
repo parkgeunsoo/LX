00780df4aa1d4cc764f774f095b7981a998df23e
park <rmstn133@gmail.com>
2024-04-11 23:58:02 +0900

    dist

---
+++ dist/assets/js/ui.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var Header = {
+  init: function init() {
+    this.aside();
+  },
+  aside: function aside() {
+    $(document).ready(function () {
+      $(".more-depth").click(function () {
+        $(this).next(".dep2").slideToggle(200);
+        $(this).toggleClass("active");
+      });
+    });
+  }
+};
+var Common = {
+  init: function init() {
+    this.select();
+    this.common();
+    this.datepickerRun();
+  },
+  select: function select() {
+    // selectbox 커스텀
+    $(".select-btn").click(function () {
+      $(this).parent().toggleClass("show");
+    });
+    $(".select-wrap .optionitem").click(function () {
+      var text = $(this).html();
+      $(this).parent().siblings(".select-btn").find("span").html(text);
+      $(this).parent().siblings(".select-btn").find("span").css("color", "#333");
+      $(".select-wrap").removeClass("show");
+    });
+  },
+  datepickerRun: function datepickerRun() {
+    $("[data-picker='date']").datepicker({
+      dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"],
+      monthNames: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+      monthNamesShort: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+      showOtherMonths: false,
+      //selectOtherMonths: true,
+      showMonthAfterYear: true,
+      yearSuffix: "년",
+      dateFormat: "yy-mm-dd"
+    });
+  },
+  common: function common() {
+    // confirm 모달
+    $(document).ready(function () {
+      // 확인 버튼 클릭 시 다른 모달 표시
+      $(".save-btn").click(function () {
+        $("#alert-save-confirm").modal("hide"); // Confirm 모달 닫기
+        $("#alert-save").modal("show"); // 다른 모달 표시
+      });
+    });
+
+    // 메뉴관리 메뉴
+    $(".menu-list span").click(function () {
+      $(this).addClass("active");
+      // 이외의 모든 span에서 active 클래스 제거
+      $(".menu-list span").not(this).removeClass("active");
+    });
+    $(".menu-depth1 > li  .icon-plus").click(function () {
+      $(this).closest("div").siblings(".menu-depth2").slideToggle("active");
+      $(this).closest("div").toggleClass("open");
+    });
+    $(".menu-depth2 > li  .icon-plus").click(function () {
+      $(this).closest("div").siblings(".menu-depth3").slideToggle("active");
+      $(this).toggleClass("open");
+    });
+    $(".open-tool").click(function () {
+      $(".tool-wrap").css("display", "flex");
+    });
+    $(document).ready(function () {
+      $("#fileInput").on("change", function () {
+        var selectedFiles = this.files;
+        if (selectedFiles.length > 0) {
+          $("#selectedFile").text(selectedFiles[0].name);
+          $("#selectedFile").addClass("select");
+        } else {
+          $("#selectedFile").text("선택된 파일이 없습니다.");
+        }
+      });
+      // 버튼 클릭 시 페이지 최상단으로 이동합니다.
+      $('.top-btn').click(function () {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 400); // 800ms 동안 최상단으로 스크롤
+        return false;
+      });
+    });
+  }
+};
+Header.init();
+Common.init();
